98c9334e8b89de1b70fe236e56ea5f863c058cfb
adityajain375 <53340053+adityajain375@users.noreply.github.com>
2020-06-23 19:09:04 +0530

    Update App.js

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
 import { createAppContainer} from 'react-navigation';
 import { createBottomTabNavigator } from 'react-navigation-tabs';
 import { render } from 'react-dom';
@@ -17,7 +17,32 @@ export default class App extends React.Component  {
 const TabNavigator = createBottomTabNavigator({
   Write: {screen: WriteScreen},
   Read: {screen: ReadScreen},
-});
+},{
+defaultNavigationOptions: ({navigation})=>({
+  tabBarIcon: ()=>{
+    const routeName = navigation.state.routeName;
+      console.log(routeName)
+      if(routeName === "Write"){
+        return(
+          <Image
+          source={require("./assets/write.jpg")}
+          style={{width:40, height:40}}
+        />
+        )
+
+      }
+      else if(routeName === "Read"){
+        return(
+          <Image
+          source={require("./assets/read.png")}
+          style={{width:40, height:40}}
+        />)
+
+      }
+  }
+})
+}
+);
 
 const AppContainer =  createAppContainer(TabNavigator);
 
